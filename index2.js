4e1c139fde8e8f7cb69927ef43984130d5da0aea
jorge-jah <jorgejah14@gmail.com>
2024-01-29 19:49:34 -0300

    commit inicial

---
+++ index2.js
@@ -0,0 +1,14 @@
+const http = require('http');
+
+const HOSTNAME = 'localhost';
+const PORT = 5000;
+
+const server = http.createServer((req, res) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('Resposta enviada!');
+});
+
+server.listen((PORT, HOSTNAME, () => {
+    console.log(`Server runnig at http://${HOSTNAME}:${PORT}`);
+}));
